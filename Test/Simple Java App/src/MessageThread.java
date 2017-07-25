bd73d225ed4205b0f3207fd5867a4e80cd6d15a6
Edwin <edwin.viljoen.1993@gmail.com>
2017-07-25 11:25:05 +0200

    Added some text to the Hello World app,  Please note I used intelliJ to make changes, so project files also added

--- Test/Simple Java App/src/MessageThread.java
+++ Test/Simple Java App/src/MessageThread.java
@@ -11,12 +11,25 @@ public class MessageThread implements Runnable {
     @Override
     public void run() {
         System.out.println("Thread Started... " + this.getClass());
+        simpleJava.setText("Hello!!");
         while (true) {
-
             System.out.println("loop");
-            simpleJava.setText("Hello!!");
             sleepFourSeconds();
             simpleJava.getLabelLabel1().setText("Please feel free to commit some changes!");
+            sleepFourSeconds();
+            simpleJava.setText("We are group 14 :)");
+            sleepFourSeconds();
+            simpleJava.setText("Edwin Viljoen");
+            sleepFourSeconds();
+            simpleJava.setText("Morné du Plooy");
+            sleepFourSeconds();
+            simpleJava.setText("Zane Schoonraad");
+            sleepFourSeconds();
+            simpleJava.setText("Stian Scholtz");
+            sleepFourSeconds();
+            simpleJava.setText("Justin vd Westhuizen");
+            sleepFourSeconds();
+            simpleJava.setText("Kristiaan Pretorius");
             sleepFourSeconds();
         }
     }
